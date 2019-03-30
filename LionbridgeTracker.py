57f805cb41f9acf16ac96a41d8c1355eacd97841
Floydasaurus <Zamarripa232@gmail.com>
2019-03-30 01:40:13 -0500

    initial commit.

---
+++ LionbridgeTracker.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""LionbridgeTracker.py: Assists in Lionbridge RaterHub timekeeping."""
+
+__author__ = "Floyd Zamarripa"
+__copyright__ = "Copyright 2019, Floydasaur.us"
+__version__ = "0.001"
+__email__ = "floyd@floydasaur.us"
+__status__ = "Prototype"
+
+#
